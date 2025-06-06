0623103403ba2fee3a7eba0b4a12515f6fb20356
anmol420 <anmolanand2k15@gmail.com>
2025-06-06 18:27:30 +0530

    Initial commit

---
+++ src/test/java/com/anmol420/blog_app/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anmol420.blog_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

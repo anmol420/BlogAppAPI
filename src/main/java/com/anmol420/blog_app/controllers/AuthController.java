9667249d33d205b19e848a0781bc3b4c5817d4c1
anmol420 <anmolanand2k15@gmail.com>
2025-06-08 01:03:22 +0530

    Added AuthenticationController

---
+++ src/main/java/com/anmol420/blog_app/controllers/AuthController.java
@@ -0,0 +1,36 @@
+package com.anmol420.blog_app.controllers;
+
+import com.anmol420.blog_app.domain.dtos.AuthResponse;
+import com.anmol420.blog_app.domain.dtos.LoginRequest;
+import com.anmol420.blog_app.services.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping(path = "/login")
+    public ResponseEntity<AuthResponse> login(
+            @RequestBody LoginRequest loginRequest
+    ) {
+       UserDetails userDetails = authenticationService.authenticate(
+               loginRequest.getEmail(),
+               loginRequest.getPassword()
+       );
+       String tokenValue = authenticationService.generateToken(userDetails);
+       AuthResponse authResponse = AuthResponse.builder()
+               .token(tokenValue)
+               .expiresIn(86400)
+               .build();
+       return ResponseEntity.ok(authResponse);
+    }
+}

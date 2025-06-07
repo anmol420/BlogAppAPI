e0ba1df20a6a8543c7caebd09727e6361749c4b1
anmol420 <anmolanand2k15@gmail.com>
2025-06-08 00:34:43 +0530

    Added User Service and Details Fetcher

---
+++ src/main/java/com/anmol420/blog_app/security/BlogUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.anmol420.blog_app.security;
+
+import com.anmol420.blog_app.domain.entities.User;
+import com.anmol420.blog_app.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found With Email: " + email));
+        return new BlogUserDetails(user);
+    }
+}

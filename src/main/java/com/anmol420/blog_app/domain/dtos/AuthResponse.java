849065b880e09d2307746ab3c3625eeb780a5580
anmol420 <anmolanand2k15@gmail.com>
2025-06-08 01:02:56 +0530

    Added Authentication DTOs

---
+++ src/main/java/com/anmol420/blog_app/domain/dtos/AuthResponse.java
@@ -0,0 +1,17 @@
+package com.anmol420.blog_app.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+    private long expiresIn;
+
+}
